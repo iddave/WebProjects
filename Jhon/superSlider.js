30b1b1963c92eed5013c5355461fc69ee1b2ee87
David <laliev2001@gmail.com>
2022-12-26 19:29:07 +0300

    final

--- Jhon/superSlider.js
+++ Jhon/superSlider.js
@@ -1,39 +1,69 @@
 let postParent = document.querySelector('.featured_works');
 let posts = postParent.children;
-console.log(posts);
-// posts[0].parentElement.hidden=true;
-for (let i = 1; i < posts.length; i++) {
-    posts[i].hidden = true;
-    posts[i].setAttribute('activePost', false);
+
+if (window.innerWidth < 769) {
+    hidePosts();
+    menu.style.display = "none";
+    console.log(`firstIf `);
 }
-posts[0].setAttribute('activePost', true);
+window.addEventListener('resize', function() {
+    if (window.innerWidth >= 769) {
+        makeAllFlex();
+        menu.style.display = "flex";
+
+    } else {
+        menu.style.display = "none";
+        hidePosts();
+    }
+});
+
+function makeAllFlex() {
+    for (let i = 0; i < posts.length; i++) {
+        posts[i].style.display = "flex";
+        posts[i].setAttribute('activePost',true);
+    }
+}
+
+function hidePosts() {
+    if (document.querySelector("[activePost='false']") == null) {
+        for (let i = 1; i < posts.length; i++) {
+            posts[i].hidden = true;
+            posts[i].style.display = "none";
+            posts[i].setAttribute('activePost', false);
+            console.log(`postInHidden = ${posts[i]}`);
+        }
+        posts[0].setAttribute('activePost', true);
+    }
+}
 
 document.addEventListener('click', function(event) {
     let current = document.querySelector("[activePost='true']")
     let prev = current.previousElementSibling;
     let last = postParent.lastElementChild;
     let first = postParent.firstElementChild;
-    let next = current.nextElementSibling; //elem!!
+    let next = current.nextElementSibling; 
     let nextToShow;
-
-    console.log(`current = ${current.innerHTML}`);
-    console.log(`currentHidden = ${current.hidden}`);
-    console.log(`currentActive = ${current.getAttribute('activePost')}`);
-
     let id = event.target.dataset.about;
-    console.log(`id = ${id}`);
-    console.log(`event.target = ${event.target}`);
     if (!id) return;
+
     current.setAttribute('activePost', false);
-    current.hidden = true;
+    current.style.display = "none";
     if (id == 'left') showNextPost(prev, last);
     else showNextPost(next, first);
-    console.log(`currentHiddenAfterClick = ${current.hidden}`);
-    console.log(`currentActiveAfterClick = ${current.getAttribute('activePost')}`);
 });
 
 function showNextPost(between, edge) {
-    nextToShow = between ? ? edge;
+    nextToShow = between ?? edge;
     nextToShow.setAttribute('activePost', true);
-    nextToShow.hidden = false;
+    nextToShow.style.display = "flex";
+}
+
+function showMenu() {
+    if (menu.style.display == "none") {
+        menu.style.display = "flex";
+        burgir.style.border = '2px solid black';
+    } else {
+        menu.style.display = "none";
+        burgir.style.border = 'none';
+    }
 }
