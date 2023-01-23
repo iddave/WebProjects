2d471373d4800e6a61fe52ebd86b5cc014699a2f
David <laliev2001@gmail.com>
2023-01-23 18:57:06 +0300

    statut added

---
+++ Statut/script.js
@@ -0,0 +1,67 @@
+$(document).ready(function(){
+  $('.discount__slider').slick({
+  	prevArrow: '<div class="prev-arrow slick-arrow"><img src="img/icons/left.png" alt=""></div>',
+  	nextArrow: '<div class="next-arrow slick-arrow"><img src="img/icons/right.png" alt=""></div>'
+  });
+});
+
+$(document).ready(function(){
+  $('.mob_discount__slider').slick({
+  	prevArrow: null,
+  	nextArrow: null
+  });
+});
+
+$('.new-arriv_slider').slick({
+	// centerMode: true,
+ //  centerPadding: '60px',
+  infinite: true,
+  prevArrow: $('.slick2-prev'),
+  nextArrow: $('.slick2-next'),
+  slidesToShow: 4,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1
+      }
+    }
+  ]
+});
+
+$('.published-soon_slider').slick({
+	// centerMode: true,
+ //  centerPadding: '60px',
+  infinite: true,
+  prevArrow: $('.slick3-prev'),
+  nextArrow: $('.slick3-next'),
+  slidesToShow: 4,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1
+      }
+    }
+  ]
+});
+
+function showCatalog() {
+    if (topCatalog.style.display != "none") {
+        topCatalog.style.display = "none";
+    } else {
+        topCatalog.style.display = "block";    }
+}
+
